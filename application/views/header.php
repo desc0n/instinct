a5c18d6a0b30d58e73f3047ace53cb26b7d86491
desc0n <desconbk@yandex.ru>
2016-07-21 23:39:57 +1000

    Вывожу кнопку регистрации и анкетирования

--- application/views/header.php
+++ application/views/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container">
-        <div class="row">
+        <div class="menu-row">
             <?foreach ($menu as $menuData) {
                 $slug = $menuData['slug'];
                 $class = $slug === $rootPage ? 'menu-item-active' : 'menu-item';
@@ -9,6 +9,21 @@
                 ?>
             <a class="<?=$class;?>" href="/<?=$slug;?>"><?=$menuData['name'];?></a>
             <?}?>
+        </div>
+        <div class="row">
+            <div class="col-lg-2 text-center">
+                <div class="question">
+                    <p>Вам есть 18 лет?</p>
+                    <p>
+                        <button class="btn btn-yes">Да</button>
+                        <button class="btn btn-no">Нет</button>
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-8"></div>
+            <div class="col-lg-2 text-center">
+                <a class="menu-item-active" href="/input">Вход и регистрация</a>
+            </div>
         </div>
     </div>
 </header>
