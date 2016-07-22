3f79fb87345181274c81e2c7d7438c451cab0561
desc0n <desconbk@yandex.ru>
2016-07-22 21:58:16 +1000

    Добавляю центральную часть заголовка

--- application/views/header.php
+++ application/views/header.php
@@ -11,7 +11,7 @@
             <?}?>
         </div>
         <div class="row">
-            <div class="col-lg-2 text-center">
+            <div class="col-lg-3 text-center">
                 <div class="question">
                     <p>Вам есть 18 лет?</p>
                     <p>
@@ -19,10 +19,28 @@
                         <button class="btn btn-no">Нет</button>
                     </p>
                 </div>
+                <div class="news-articles">
+                    <h2>Новости</h2>
+                </div>
             </div>
-            <div class="col-lg-8"></div>
-            <div class="col-lg-2 text-center">
-                <a class="menu-item-active" href="/input">Вход и регистрация</a>
+            <div class="col-lg-6 text-center">
+                <img class="header-img" src="/public/i/header_img.png">
+                <h1 class="header-phone">Тел. 0-00-00-00</h1>
+                <div class="header-search">
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+                        </span>
+                    </div>
+                    <button class="btn btn-warning">АКЦИЯ!</button>
+                </div>
+            </div>
+            <div class="col-lg-3 text-center">
+                <div class="btn-input" href="/input">Вход и регистрация</div>
+                <div class="news-articles">
+                    <h2>Статьи</h2>
+                </div>
             </div>
         </div>
     </div>
