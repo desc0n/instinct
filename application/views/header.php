9d41ea54e0ba5510e00589de71ac246072328a3d
desc0n <desconbk@yandex.ru>
2016-08-01 22:21:41 +1000

    Устраняю ошибку с кнопкой в меню на главную страницу

--- application/views/header.php
+++ application/views/header.php
@@ -5,7 +5,7 @@
                 $slug = '/page/' . $menuData['slug'];
                 $class = $slug === $rootPage ? 'menu-item-active' : 'menu-item';
 
-                $slug = str_replace('/page/main', '', $slug);
+                $slug = str_replace('page/main', '', $slug);
                 ?>
             <a class="<?=$class;?>" href="<?=$slug;?>"><?=$menuData['name'];?></a>
             <?}?>
