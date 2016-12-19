ab3707b1c18a44a4a85243c339f35f1cfc104b20
desc0n <desconbk@yandex.ru>
2016-12-19 21:18:39 +1000

    Редактирую админку

--- application/views/admin_redact_notice.php
+++ application/views/admin_redact_notice.php
@@ -1,3 +1,10 @@
+<?
+/** @var $contentModel Model_Content */
+$contentModel = Model::factory('Content');
+
+/** @var Model_Notice $noticeModel */
+$noticeModel = Model::factory('Notice');
+?>
 <div class="row">
     <h2 class="sub-header col-sm-12">Редактирование товаров:</h2>
     <h4 class="sub-header col-sm-12">Группа: <?=Arr::get($notice_info,'category_name');?></h4>
@@ -53,7 +60,7 @@
             </tr>
         </table>
         <div class="col-md-12 form-group">
-            <button class="btn btn-success" onclick="$('#redactproduct_form').submit();">Сохранить</button>
+            <button class="btn btn-success" onclick="$('#redactproduct_form').submit();">Сохранить изменения</button>
         </div>
         <div class="col-md-12 form-group">
             <form method="post" class="form-horizontal" action="/admin/control_panel/add_notice">
@@ -61,6 +68,9 @@
                 <input type="hidden" class="form-control" name="name">
                 <input type="hidden" name="category" value="<?=Arr::get($notice_info,'category');?>">
             </form>
+        </div>
+        <div class="col-md-12 form-group">
+            <button class="btn btn-primary"  onclick="$('#addCustomCategoryNotice').modal('toggle');">Добавить в другую категорию <span class="glyphicon glyphicon-plus"></span> </button>
         </div>
     </div>
 </div>
@@ -114,3 +124,56 @@
 <form id="remove_file" method="post">
     <input type="hidden" id="removefile" name="removefile" value="0">
 </form>
+<div class="modal fade" id="addCustomCategoryNotice">
+    <form role="form" method="post" action="/admin/control_panel/add_notice">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Выберите категорию для добавления</h4>
+            </div>
+            <div class="modal-body">
+                <?foreach($contentModel->getCategory() as $group_1_data){?>
+                    <div class="row-accordion">
+                        <div class="panel-group" id="accordion1<?=$group_1_data['id'];?>">
+                            <div class="row">
+                                <div class="col-lg-1 col-sm-1 col-md-1 col-xs-1">
+                                    <input type="radio" name="category" value="<?=$group_1_data['id'];?>">
+                                </div>
+                                <a class="col-lg-7 col-sm-7 col-md-7 col-xs-7" data-toggle="collapse" data-parent="#accordion1<?=$group_1_data['id'];?>" href="#collapse1<?=$group_1_data['id'];?>">
+                                    <?=$group_1_data['name'];?>
+                                </a>
+                                <div class="text-left col-lg-4 col-sm-4 col-md-4 col-xs-4">
+                                    <span class="glyphicons glyphicons-collapse"></span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div id="collapse1<?=$group_1_data['id'];?>" class="col-lg-12 col-sm-12 col-md-12 col-xs-12 panel-collapse collapse <?=(Arr::get($get,'group_1','') == $group_1_data['id'] ? 'in' : '');?>">
+                                    <?foreach ($contentModel->getCategory($group_1_data['id']) as $group_2_data){?>
+                                    <div class="row-accordion">
+                                        <div class="row" id="accordion2<?=$group_2_data['id'];?>">
+                                            <div class="col-lg-1 col-sm-1 col-md-1 col-xs-1"></div>
+                                            <div class="col-lg-1 col-sm-1 col-md-1 col-xs-1">
+                                                <input type="radio" name="category" value="<?=$group_2_data['id'];?>">
+                                            </div>
+                                            <a class="col-lg-8 col-sm-8 col-md-8 col-xs-8" data-toggle="collapse" data-parent="#accordion2<?=$group_2_data['id'];?>" href="#collapse2<?=$group_2_data['id'];?>">
+                                                <?=$group_2_data['name'];?>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <?}?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?}?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Закрыть</button>
+                <button class="btn btn-primary" type="submit" name="addnotice" value="3">Добавить <span class="glyphicon glyphicon-plus"></span></button>
+                <input type="hidden" class="form-control" name="name">
+            </div>
+        </div>
+    </div>
+    </form>
+</div>
